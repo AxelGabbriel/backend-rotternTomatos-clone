b46f1acdf7e4051c9212481247382ab809106799
AxelGabbriel <axelmiquilena@gmail.com>
2022-10-24 19:02:15 -0400

    ...

--- src/database.js
+++ src/database.js
@@ -49,9 +49,9 @@ const  {
    //funcion buscar like
    const buscarlike=async(req,res)=>{
     const id_resena=req.params.id_resena
-    const response= await pool.query('select count(like_id) from liked where id_resena=2',[id_resena])
-    console.log(response.rowCount);
-    res.json(response.rowCount);
+    const response= await pool.query('select count(like_id) from liked where id_resena=$1',[id_resena])
+    console.log(response);
+    res.json(response);
 }
     //borrar like 
     const borrarlike= async(req,res)=>{
