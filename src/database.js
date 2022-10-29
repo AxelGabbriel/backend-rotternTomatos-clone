f69337dc360f3f0140d3b79f92afe520b9d4a775
AxelGabbriel <axelmiquilena@gmail.com>
2022-10-29 02:40:27 -0400

    fuap

--- src/database.js
+++ src/database.js
@@ -148,15 +148,15 @@ const crearreseña= async(req,res)=>{
         const  { 
                 id_resena,
                 fecha,
-                comentario,
-                 username
+                comentarios,
+                username
              
               }= req.body;
       
-             const result= await pool.query('INSERT INTO comentario(id_resena,fecha,comentario,username) VALUES($1,$2,$3,$4)', [
+             const result= await pool.query('INSERT INTO comentario(id_resena,fecha,comentarios,username) VALUES($1,$2,$3,$4)', [
              id_resena,fecha,comentario,username ])
-              console.log(result)
-              res.json(result.rows)
+             console.log(result)
+             res.json(result.rows)
         
               
             }
